96322877bb16447c04982be4312cff5b12b6588e
Mohammad Aasif <mdaasifxii@gmail.com>
2024-03-23 16:28:56 +0530

    Files updated

--- app.py
+++ app.py
@@ -76,14 +76,14 @@ class House(Base):
     residence_type = Column(String(50)) # Type of room required eg., Single room, Sharing Room , flat etc
     num_bathrooms = Column(Integer) # Numbeer of bathroom
     attached_kitchen = Column(String(3)) # It is yes or no answer so we will save 1 or 0
-    # shopping_mall = Column(Integer) # It is yes or no answer so we will save 1 or 0
-    # num_beds = Column(Integer) # Number of besd in a room
-    # transport_facility = Column(Integer) # It is yes or no answer so we will save 1 or 0x
-    # medical_shops = Column(Integer) # It is yes or no answer so we will save 1 or 0
-    # num_food_mess = Column(Integer) # Number of Mess available nearby
-    # time_to_market = Column(Integer) # Time taken to reach market on foot 5, 10, 15, 20, ... minutes
-    # time_to_college = Column(Integer) # Time taken to reach College on foot 5, 10, 15, 20, ... minutes
-    # playground = Column(Integer)# It is yes or no answer so we will save 1 or 0
+    shopping_mall = Column(Integer) # It is yes or no answer so we will save 1 or 0
+    num_beds = Column(Integer) # Number of besd in a room
+    transport_facility = Column(Integer) # It is yes or no answer so we will save 1 or 0x
+    medical_shops = Column(Integer) # It is yes or no answer so we will save 1 or 0
+    num_food_mess = Column(Integer) # Number of Mess available nearby
+    time_to_market = Column(Integer) # Time taken to reach market on foot 5, 10, 15, 20, ... minutes
+    time_to_college = Column(Integer) # Time taken to reach College on foot 5, 10, 15, 20, ... minutes
+    playground = Column(Integer)# It is yes or no answer so we will save 1 or 0
 
     def __repr__(self):
         return f'<House {self.id}>'
@@ -160,24 +160,27 @@ card_data = [
         'playground': 'Available'
     }
 ]
+houses = House.query.all()
+
+
 class HouseForm(FlaskForm):
     image = StringField('Image Link', validators=[DataRequired()])
     description = TextAreaField('Description', validators=[DataRequired(), Length(max=255)])
-    # add_date = StringField('Registration Date', validators=[DataRequired()])
+    add_date = StringField('Registration Date', validators=[DataRequired()])
     owner_name = StringField('Owner Name', validators=[DataRequired(), Length(max=255)])
     contact_number = StringField('Contact Number', validators=[DataRequired(), Length(max=15)])
     monthly_rent = IntegerField('Monthly Rent', validators=[DataRequired(), NumberRange(min=0)])
     residence_type = SelectField('Residence Type', choices=[('Single Room', 'Single Room'), ('Sharing Room', 'Sharing Room'), ('Flat', 'Flat')], validators=[DataRequired()])
     num_bathrooms = IntegerField('Number of Bathrooms', validators=[DataRequired(), NumberRange(min=0)])
     attached_kitchen = BooleanField('Attached Kitchen')
-    # shopping_mall = BooleanField('Shopping Mall Nearby')
-    # num_beds = IntegerField('Number of Beds', validators=[DataRequired(), NumberRange(min=0)])
-    # transport_facility = BooleanField('Transport Facility Nearby')
-    # medical_shops = BooleanField('Medical Shops Nearby')
-    # num_food_mess = IntegerField('Number of Food Mess Nearby', validators=[DataRequired(), NumberRange(min=0)])
-    # time_to_market = IntegerField('Time to Reach Market (minutes)', validators=[DataRequired(), NumberRange(min=0)])
-    # time_to_college = IntegerField('Time to Reach College (minutes)', validators=[DataRequired(), NumberRange(min=0)])
-    # playground = BooleanField('Playground Nearby')
+    shopping_mall = BooleanField('Shopping Mall Nearby')
+    num_beds = IntegerField('Number of Beds', validators=[DataRequired(), NumberRange(min=0)])
+    transport_facility = BooleanField('Transport Facility Nearby')
+    medical_shops = BooleanField('Medical Shops Nearby')
+    num_food_mess = IntegerField('Number of Food Mess Nearby', validators=[DataRequired(), NumberRange(min=0)])
+    time_to_market = IntegerField('Time to Reach Market (minutes)', validators=[DataRequired(), NumberRange(min=0)])
+    time_to_college = IntegerField('Time to Reach College (minutes)', validators=[DataRequired(), NumberRange(min=0)])
+    playground = BooleanField('Playground Nearby')
 
 @app.route('/submit_contact_form', methods=['POST'])
 def submit_contact_form():
@@ -297,21 +300,21 @@ def add_house():
             id = random.randint(1,100),
             image=form.image.data,
             description=form.description.data,
-            # add_date=form.add_date.data,
+            add_date=form.add_date.data,
             owner_name=form.owner_name.data,
             contact_number=form.contact_number.data,
             monthly_rent=form.monthly_rent.data,
             residence_type=form.residence_type.data,
             num_bathrooms=form.num_bathrooms.data,
             attached_kitchen=form.attached_kitchen.data,
-            # shopping_mall=form.shopping_mall.data,
-            # num_beds=form.num_beds.data,
-            # transport_facility=form.transport_facility.data,
-            # medical_shops=form.medical_shops.data,
-            # num_food_mess=form.num_food_mess.data,
-            # time_to_market=form.time_to_market.data,
-            # time_to_college=form.time_to_college.data,
-            # playground=form.playground.data
+            shopping_mall=form.shopping_mall.data,
+            num_beds=form.num_beds.data,
+            transport_facility=form.transport_facility.data,
+            medical_shops=form.medical_shops.data,
+            num_food_mess=form.num_food_mess.data,
+            time_to_market=form.time_to_market.data,
+            time_to_college=form.time_to_college.data,
+            playground=form.playground.data
         )
         Session = sessionmaker(bind=engine)
         session = Session()
@@ -323,6 +326,8 @@ def add_house():
 @app.route('/success')
 def success():
     return 'House added successfully!'
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
